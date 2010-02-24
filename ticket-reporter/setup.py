3bd01a9287e865167d5624b5fb7345b44b20cb8b
Thomas Hervé <thomas@canonical.com>
2010-02-24 13:46:10 -0500

    Add entry-points

--- ticket-reporter/setup.py
+++ ticket-reporter/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
+    entry_points={'trac.plugins': "IRC REPORT TICKET MAN = ticket_reporter"},
     name='IRC REPORT TICKET MAN', version='ONE',
     description='REPORT TICKET CHANGE ON IRC CHANNEL',
     author="JEAN-PAUL CALDERONE", author_email="DO NOT SEND ME EMAIL I PROBABLY HATE YOU",
